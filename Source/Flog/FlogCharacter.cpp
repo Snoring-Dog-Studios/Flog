ebd01d7f6d9d8b26be353ff2b8c193165cec91c8
Vicente Pinana <80524044+VicentePinana@users.noreply.github.com>
2023-12-19 15:55:06 -0300

    Test Map

--- Source/Flog/FlogCharacter.cpp
+++ Source/Flog/FlogCharacter.cpp
@@ -32,9 +32,9 @@ AFlogCharacter::AFlogCharacter()
 
 	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
 	// instead of recompiling to adjust them
-	GetCharacterMovement()->JumpZVelocity = 700.f;
-	GetCharacterMovement()->AirControl = 0.35f;
-	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->JumpZVelocity = 800.f;
+	GetCharacterMovement()->AirControl = 0.45f;
+	GetCharacterMovement()->MaxWalkSpeed = 800.f;
 	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
